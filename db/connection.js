b4a71d9294075c35f1e07a2f06acdd9b7d1d0188
Fahim Kabir <fkabir101@gmail.com>
2019-02-06 11:45:20 -0500

    see if it works

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql");
-require("dotenv").config();
+//require("dotenv").config();
 
 let connection;
+var connection;
 
-if(process.env.JAWSDB_URL){
+// Sets up db to connect locally or on JAWSDB if deployed
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else{
+} else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
-    password: process.env.dbPassword,
-    database: "note_db"
+    password: "nightmare10",
+    //password: process.env.dbPassword,
+    database: "notes_db"
   });
 }
 module.exports = connection;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
